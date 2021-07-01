b31cc7ac1978c365ff90ece712997ff1ffc3dd30
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-07-01 23:40:32 +0200

    I continued the project

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,12 @@
 import express from "express";
+import RestaurantsController from "./restaurants.controller.js"
 
 const router = express.Router(); //because this is a route file. it will redirect people.
 
-router.route('/').get((req, res) => {
-    res.send("hello world")
-})
+// router.route('/').get((req, res) => {
+//     res.send("hello world")
+// })
+
+router.route("/").get(RestaurantsController.apiGetRestaurants);
 
 export default router;
