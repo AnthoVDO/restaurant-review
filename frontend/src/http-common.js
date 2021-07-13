6f253de3a97f011ccfd488ff1046595f7ff02abe
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-07-13 19:23:58 +0200

    changed the url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+const url = "https://back-end-restaurant-review.herokuapp.com/api/v1/restaurants" || "http://localhost:8080/api/v1/restaurants"
 
 export default axios.create({
-    baseURL:"http://localhost:8080/api/v1/restaurants",
+    baseURL:url,
     Headers:{
         "Content-type": "application/json"
     }
