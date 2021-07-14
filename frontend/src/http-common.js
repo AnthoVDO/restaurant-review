d8326a0d3ed55e38739bedddcfab4f1d5524343d
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-07-14 18:31:35 +0200

    I changed the build to pass vercell error

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const url = "https://back-end-restaurant-review.herokuapp.com/api/v1/restaurants" || "http://localhost:8080/api/v1/restaurants"
+ const url = "https://back-end-restaurant-review.herokuapp.com/api/v1/restaurants" || "http://localhost:8080/api/v1/restaurants"
+// const url = "http://localhost:8080/api/v1/restaurants"
+// const url = "https://back-end-restaurant-review.herokuapp.com/api/v1/restaurants"
 
 export default axios.create({
     baseURL:url,
